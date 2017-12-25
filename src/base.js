2446611f503983aff4859b708023c8c6bc195af6
ibrahim <duranteau.l.i@gmail.com>
2017-12-25 12:14:21 +0000

    firebase

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyDlaFe_BzF4KhzTKVml-zewuvBQeZndCG0",
+    authDomain: "catch-of-the-day-666bd.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-666bd.firebaseio.com"
+});
+
+export default base;
